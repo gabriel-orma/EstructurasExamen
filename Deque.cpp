78d5e756cf0b82527e627344e10c00282f5c6df3
Gabriel Ormachea Mamani <gabriel.ormachea@utec.edu.pe>
2026-09-05 00:46:34 -0500

    Fix formatting of head and tail initialization

--- Deque.cpp
+++ Deque.cpp
@@ -3,7 +3,7 @@ const int MAX = 1000005;
 
 struct Deque {
     int arr[MAX * 2];
-    int head = MAX, tail = MAX; // Empiezan en el centro
+    int head = MAX, tail = MAX;
 
     void push_back(int val) { arr[tail++] = val; }
     void push_front(int val) { arr[--head] = val; }
